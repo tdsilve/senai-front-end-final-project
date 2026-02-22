edfdeb3c9f7d8ca5b957e625d7ad6c868d536603
Thais <thaisdsilve@gmail.com>
2026-02-22 20:00:07 -0300

    fix: update PostImage component to use onLoad event and improve image styling

--- components/posts/post-image.tsx
+++ components/posts/post-image.tsx
@@ -8,9 +8,9 @@ export const PostImage = ({ src }: { src: string }) => {
         src={src}
         fill
         alt="issue cover image"
-        className="opacity-0 duration-[2s] transition-opacity"
-        onLoadingComplete={(e) => {
-          e.classList.remove("opacity-0");
+        className="opacity-0 duration-[2s] transition-opacity object-contain"
+        onLoad={(e) => {
+          e.currentTarget.classList.remove("opacity-0");
         }}
       />
     </div>
